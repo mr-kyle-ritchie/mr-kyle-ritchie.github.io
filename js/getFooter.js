56eb625d174717d0da7aa6b802c5ef4a2d1e7a99
mr-kyle-ritchie <mr.kyle.ritchie@gmail.com>
2016-04-03 22:47:42 -0400

    Week 8-9 partially done

---
+++ js/getFooter.js
@@ -0,0 +1,10 @@
+$.get("http://liam013.github.io/partials/nav.html", function(data){
+    
+    $(document).ready(function(){
+    $("footer").show();
+    $("footer").style.backgroundColor = 'transparent';
+    $(".container").append.getElementsByClassName("footer");
+    
+    })
+
+})
